590b47271ec51a609a03430102e711dac8325d5a
MvdS <git@vandersluys.nl>
2010-12-30 15:28:33 +0000

    Default CMake files without run-time checks

--- CMake/SetCompileOptions.cmake
+++ CMake/SetCompileOptions.cmake
@@ -2,12 +2,12 @@
 
 option( CMAKE_VERBOSE_MAKEFILE "Verbose makefile" off )
 
+option( WANT_CHECKS    "Activate runtime checks (array bounds, NaNs)" off )
+option( WANT_WARNINGS  "Activate warnings" on )
 option( WANT_OPENMP    "Use OpenMP parallelisation (experimental)" off )
 option( WANT_SSE42     "Enable generation of SSE4.2 code" off )
 option( WANT_HOST_OPT  "Enable host-specific optimisation. Choose only when compiling and running on the same machine! Overrides WANT_SSE42" off )
 option( WANT_IPO       "Inter-procedural optimisation" off )
 option( WANT_STATIC    "Generate statically linked executable" off )
-option( WANT_CHECKS    "Activate runtime checks (array bounds, NaNs)" on )
-option( WANT_WARNINGS  "Activate warnings" on )
 option( WANT_LIBRARY   "Use compiler options to create a library" on )
 
