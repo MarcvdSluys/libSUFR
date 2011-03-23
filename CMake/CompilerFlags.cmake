48b20461fa2ee4854f3269468218e4baf9d61f08
MvdS <git@vandersluys.nl>
2011-03-23 17:17:02 +0000

    Updated CMake files, make

--- CMake/CompilerFlags.cmake
+++ CMake/CompilerFlags.cmake
@@ -9,7 +9,9 @@ set(MOD_FLAGS "${INCLUDE_FLAGS}" )
 get_filename_component( Fortran_COMPILER_NAME ${CMAKE_Fortran_COMPILER} NAME )
 
 
-# Specific options per compiler:
+######################################################################################################################################################
+#  Specific options per compiler:
+######################################################################################################################################################
 if( Fortran_COMPILER_NAME MATCHES "gfortran" )
   
   set( CMAKE_Fortran_FLAGS_ALL "-std=f2008 -fall-intrinsics -pedantic" )               # v.4.4
@@ -30,28 +32,58 @@ if( Fortran_COMPILER_NAME MATCHES "gfortran" )
   
   if( WANT_STATIC )
     set( STATIC_FLAGS "-static" )
-    message( STATUS "Linking statically" )
   endif( WANT_STATIC )
   
   if( WANT_CHECKS )
     #set( CHECK_FLAGS "-O0 -fbounds-check -ffpe-trap=zero,invalid -fsignaling-nans -fbacktrace" ) # v.4.4
     set( CHECK_FLAGS "-O0 -fcheck=all -ffpe-trap=zero,invalid -fsignaling-nans -fbacktrace" )  # From v.4.5
-    message( STATUS "Compiling with run-time checks" )
   else( WANT_CHECKS )
     set( CHECK_FLAGS "-O2" )
   endif( WANT_CHECKS )
   
   if( WANT_WARNINGS )
     set( WARN_FLAGS "-Wall -Wextra" )
-    message( STATUS "Compiling with warnings" )
   endif( WANT_WARNINGS )
   
   if( WANT_LIBRARY )
     set( LIB_FLAGS "-fPIC -g" )
-    message( STATUS "Compiling with library options" )
   endif( WANT_LIBRARY )
   
   
+  # Package-specific flags:
+  set( PACKAGE_FLAGS "" )
+  
+  
+  ####################################################################################################################################################
+elseif( Fortran_COMPILER_NAME MATCHES "g95" )
+  
+  
+  set( CMAKE_Fortran_FLAGS "" )
+  set( CMAKE_Fortran_FLAGS_RELEASE "-O2" )
+  set( CMAKE_Fortran_FLAGS_DEBUG "-O0 -g" )
+  
+  if( WANT_CHECKS )
+    set( CHECK_FLAGS "-O0 -fbounds-check -ftrace=full" )
+  else( WANT_CHECKS )
+    set( CHECK_FLAGS "-O2 -fshort-circuit" )
+  endif( WANT_CHECKS )
+  
+  if( WANT_WARNINGS )
+    # 102: module procedure not referenced,  136: module variable not used,  165: implicit interface
+    set( WARN_FLAGS "-Wall -Wextra -Werror -Wno=102,136,165" )
+    set( WARN_FLAGS "-std=f2003 ${WARN_FLAGS}" )
+  endif( WANT_WARNINGS )
+  
+  if( WANT_LIBRARY )
+    set( LIB_FLAGS "-fPIC -g" )
+  endif( WANT_LIBRARY )
+  
+  
+  # Package-specific flags:
+  set( PACKAGE_FLAGS "" )
+  
+  
+  ####################################################################################################################################################
 elseif( Fortran_COMPILER_NAME MATCHES "ifort" )
   
   
@@ -80,53 +112,28 @@ elseif( Fortran_COMPILER_NAME MATCHES "ifort" )
   
   if( WANT_STATIC )
     set( STATIC_FLAGS "-static" )
-    message( STATUS "Linking statically" )
   endif( WANT_STATIC )
   
   if( WANT_CHECKS )
     set( CHECK_FLAGS "-O0 -ftrapuv -check all -check noarg_temp_created -traceback" )
-    message( STATUS "Compiling with run-time checks" )
   else( WANT_CHECKS )
     set( CHECK_FLAGS "-O2" )
   endif( WANT_CHECKS )
   
   if( WANT_WARNINGS )
     set( WARN_FLAGS "-warn all" )
-    message( STATUS "Compiling with warnings" )
   endif( WANT_WARNINGS )
   
   if( WANT_LIBRARY )
     set( LIB_FLAGS "-fPIC -g" )
-    message( STATUS "Compiling with library options" )
   endif( WANT_LIBRARY )
   
   
-elseif( Fortran_COMPILER_NAME MATCHES "g95" )
+  # Package-specific flags:
+  set( PACKAGE_FLAGS "" )
   
   
-  set( CMAKE_Fortran_FLAGS "" )
-  set( CMAKE_Fortran_FLAGS_RELEASE "-O2" )
-  set( CMAKE_Fortran_FLAGS_DEBUG "-O0 -g" )
-  
-  if( WANT_CHECKS )
-    set( CHECK_FLAGS "-O0 -fbounds-check -ftrace=full" )
-    message( STATUS "Compiling with run-time checks" )
-  else( WANT_CHECKS )
-    set( CHECK_FLAGS "-O2 -fshort-circuit" )
-  endif( WANT_CHECKS )
-  
-  if( WANT_WARNINGS )
-    #set( WARN_FLAGS "-std=f2003 -Wall -Wobsolescent -Wunused-parameter -Wunused-internal-procs -Wunused-types -Wmissing-intent" )
-    set( WARN_FLAGS "-std=f2003 -Wall -Wextra -Werror -Wno=102,112,136,165" )
-    message( STATUS "Compiling with warnings" )
-  endif( WANT_WARNINGS )
-  
-  if( WANT_LIBRARY )
-    set( LIB_FLAGS "-fPIC -g" )
-    message( STATUS "Compiling with library options" )
-  endif( WANT_LIBRARY )
-  
-  
+  ####################################################################################################################################################
 else( Fortran_COMPILER_NAME MATCHES "gfortran" )
   
   
@@ -138,11 +145,21 @@ else( Fortran_COMPILER_NAME MATCHES "gfortran" )
   set( CMAKE_Fortran_FLAGS_DEBUG "-O0 -g" )
   
   
+  # Package-specific flags:
+  set( PACKAGE_FLAGS "" )
+  
+  
 endif( Fortran_COMPILER_NAME MATCHES "gfortran" )
+######################################################################################################################################################
 
 
 
-set( USER_FLAGS "${LIB_FLAGS} ${CHECK_FLAGS} ${WARN_FLAGS} ${SSE_FLAGS} ${IPO_FLAGS} ${OPENMP_FLAGS} ${STATIC_FLAGS} ${MOD_FLAGS}" )
+
+######################################################################################################################################################
+#  Put everything together:
+######################################################################################################################################################
+
+set( USER_FLAGS "${LIB_FLAGS} ${CHECK_FLAGS} ${WARN_FLAGS} ${SSE_FLAGS} ${IPO_FLAGS} ${OPENMP_FLAGS} ${STATIC_FLAGS} ${MOD_FLAGS} ${PACKAGE_FLAGS}" )
 
 set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS_ALL} ${CMAKE_Fortran_FLAGS} ${USER_FLAGS}" )
 set( CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_ALL} ${CMAKE_Fortran_FLAGS_RELEASE} ${USER_FLAGS}" )
@@ -150,6 +167,29 @@ set( CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_ALL} ${CMAKE_Fortran_FLA
 set( CMAKE_Fortran_FLAGS_RELWITHDEBINFO "${CMAKE_Fortran_FLAGS_RELEASE} -g" )
 
 
+
+
+######################################################################################################################################################
+#  Report what's going on:
+######################################################################################################################################################
+
 message( STATUS "Using Fortran compiler: " ${Fortran_COMPILER_NAME} " (" ${CMAKE_Fortran_COMPILER}")" )
+
+if( WANT_CHECKS )
+  message( STATUS "Compiling with run-time checks:  ${CHECK_FLAGS}" )
+endif( WANT_CHECKS )
+if( WANT_WARNINGS )
+  message( STATUS "Compiling with warnings:  ${WARN_FLAGS}" )
+endif( WANT_WARNINGS )
+if( WANT_LIBRARY )
+  message( STATUS "Compiling with library options:  ${LIB_FLAGS}" )
+endif( WANT_LIBRARY )
+if( WANT_STATIC )
+  message( STATUS "Linking statically:  ${STATIC_FLAGS}" )
+endif( WANT_STATIC )
+
+
 message( STATUS "Compiler flags used:  ${CMAKE_Fortran_FLAGS}" )
+
+
 
