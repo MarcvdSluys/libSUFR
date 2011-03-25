4329277acd2ea3ae55d5d1e1e0aaf0d8c6850902
MvdS <git@vandersluys.nl>
2011-03-25 15:25:11 +0000

    Fixed CREATE_SHAREDLIB, CREATE_STATICLIB issue,   *** SF release 0.1.5 ***

--- CMake/SetCompileOptions.cmake
+++ CMake/SetCompileOptions.cmake
@@ -2,15 +2,14 @@
 
 option( CMAKE_VERBOSE_MAKEFILE "Verbose makefile" off )
 
-option( WANT_CHECKS    "Activate runtime checks (array bounds, NaNs)" off )
-option( WANT_WARNINGS  "Activate warnings" on )
-option( WANT_OPENMP    "Use OpenMP parallelisation (experimental)" off )
-option( WANT_SSE42     "Enable generation of SSE4.2 code" off )
-option( WANT_HOST_OPT  "Enable host-specific optimisation. Choose only when compiling and running on the same machine! Overrides WANT_SSE42" off )
-option( WANT_IPO       "Inter-procedural optimisation" off )
-option( WANT_STATIC    "Generate statically linked executable" off )
-option( WANT_LIBRARY   "Use compiler options to create libraries" on )
-option( WANT_SHARED    "Create shared libraries" on )
-option( WANT_STATIC    "Create static libraries" off )
-
+option( WANT_CHECKS       "Activate runtime checks (array bounds, NaNs)" off )
+option( WANT_WARNINGS     "Activate warnings" on )
+option( WANT_OPENMP       "Use OpenMP parallelisation (experimental)" off )
+option( WANT_SSE42        "Enable generation of SSE4.2 code" off )
+option( WANT_HOST_OPT     "Enable host-specific optimisation. Choose only when compiling and running on the same machine! Overrides WANT_SSE42" off )
+option( WANT_IPO          "Inter-procedural optimisation" off )
+option( WANT_STATIC       "Generate statically linked executable" off )
+option( WANT_LIBRARY      "Use compiler options to create libraries" on )
+option( CREATE_SHAREDLIB  "Create shared libraries" on )
+option( CREATE_STATICLIB  "Create static libraries" on )
 
