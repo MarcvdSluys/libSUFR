5583096a29199d9912e83ebde53ef5b80828e143
MvdS <git@vandersluys.nl>
2012-01-14 19:58:22 +0100

    * sorting.f90:   * added subroutine sort_array_real()   * use more intelligible variables in sort_string_array()

--- src/sorting.f90
+++ src/sorting.f90
@@ -37,10 +37,10 @@ contains
   !! \retval index_list   List with indices of array values, sorted to ascending value.  
   !!                      array(index_list) gives the sorted array (int).
   !!
-  !! \note This routine does not need to be called directly, but is implicitly called by sort_array().
-  !! \see Numerical Recipes in Fortran 77, Sect.8.4.
+  !! \note  This routine does not need to be called directly, but is implicitly called by sort_array().
+  !! \see   Numerical Recipes in Fortran 77, Sect.8.4.
   
-  subroutine sorted_index_list(n,array,index_list)
+  subroutine sorted_index_list(n, array, index_list)
     use SUFR_kinds, only: double
     
     implicit none
@@ -48,7 +48,7 @@ contains
     real(double), intent(in) :: array(n)
     integer, intent(out) :: index_list(n)
     
-    integer, parameter :: m=7,nstack=50
+    integer, parameter :: m=7, nstack=50
     real(double) :: a
     integer :: i,index_i,ir,itemp,j,jstack,k,l,istack(nstack)
     
@@ -60,7 +60,8 @@ contains
     l = 1
     ir = n
     
-1   if(ir-l.lt.m) then
+1   continue
+    if(ir-l.lt.m) then
        
        do j=l+1,ir
           index_i = index_list(j)
@@ -70,13 +71,16 @@ contains
              index_list(i+1) = index_list(i)
           end do
           i = l-1
-2         index_list(i+1) = index_i
+          
+2         continue
+          index_list(i+1) = index_i
        end do
        
        if(jstack.eq.0) return
+       
        ir = istack(jstack)
        l = istack(jstack-1)
-       jstack = jstack-2
+       jstack = jstack - 2
        
     else
        
@@ -90,11 +94,13 @@ contains
           index_list(l) = index_list(ir)
           index_list(ir) = itemp
        end if
+       
        if(array(index_list(l+1)).gt.array(index_list(ir))) then
           itemp = index_list(l+1)
           index_list(l+1) = index_list(ir)
           index_list(ir) = itemp
        end if
+       
        if(array(index_list(l)).gt.array(index_list(l+1))) then
           itemp = index_list(l)
           index_list(l) = index_list(l+1)
@@ -113,15 +119,20 @@ contains
 4      continue
        j = j-1
        if(array(index_list(j)).gt.a) goto 4
+       
        if(j.lt.i) goto 5
+       
        itemp = index_list(i)
        index_list(i) = index_list(j)
        index_list(j) = itemp
+       
        goto 3
        
-5      index_list(l+1) = index_list(j)
+5      continue
+       index_list(l+1) = index_list(j)
        index_list(j) = index_i
-       jstack = jstack+2
+       jstack = jstack + 2
+       
        if(jstack.gt.nstack) write(0,'(A)')' sorted_index_list():  nstack is too small'
        
        if(ir-i+1.ge.j-l) then
@@ -137,6 +148,7 @@ contains
     end if
     
     goto 1
+    
   end subroutine sorted_index_list
   !*********************************************************************************************************************************
   
@@ -146,6 +158,7 @@ contains
   !> \brief  Sort an array to ascending value.
   !!
   !! \param array  Input: array to be sorted, output: sorted array (double)
+  !!
   !! \note Uses sorted_index_list()
   
   subroutine sort_array(array)
@@ -168,33 +181,59 @@ contains
   
   
   !*********************************************************************************************************************************
+  !> \brief  Sort an array to ascending value - single-precision wrapper for sort_array
+  !!
+  !! \param array  Input: array to be sorted, output: sorted array (double)
+  !! 
+  !! \todo  Check whether array = array(index_list) works
+  
+  subroutine sort_array_real(array)
+    use SUFR_kinds, only: double
+    
+    implicit none
+    real, intent(inout) :: array(:)
+    
+    real :: array1(size(array))
+    integer :: index_list(size(array)), n
+    
+    n = size(array)
+    call sorted_index_list(n, dble(array), index_list)
+    array1 = array
+    array = array1(index_list)  ! CHECK: would array = array(index_list) work?
+    
+  end subroutine sort_array_real
+  !*********************************************************************************************************************************
+  
+  
+  
+  !*********************************************************************************************************************************
   !> \brief  Sort strings alphabetically
   !!
-  !! \param  n     Number of strings in array str
-  !! \param  len   Length of the strings in array str
-  !! \param  str   Array of n strings with length len
-  !! \retval indx  Sorting index
+  !! \param  narr        Number of strings in array str
+  !! \param  strlen      Length of the strings in array str
+  !! \param  strarr      Array of n strings with length len
+  !! \retval index_list  Sorting index
   
-  subroutine sort_string_array(n,len,str,indx)
+  subroutine sort_string_array(narr, strlen, strarr, index_list)
     implicit none
-    integer, intent(in) :: n, len
-    character, intent(in) :: str(n)*(len)
-    integer, intent(out) :: indx(n)
+    integer, intent(in) :: narr, strlen
+    character, intent(in) :: strarr(narr)*(strlen)
+    integer, intent(out) :: index_list(narr)
     
     integer :: i,l,ic
-    real :: score(n)
+    real :: score(narr)
     
-    do i=1,n
+    do i=1,narr
        score(i) = 0.
-       do l=1,len
-          ic = ichar(str(i)(l:l))
+       do l=1,strlen
+          ic = ichar(strarr(i)(l:l))
           if(ic.ge.97.and.ic.le.122) ic = ic-32
           ic = ic-32                                       ! Limits values between 1 and 64
-          score(i) = score(i) + real(ic) * 64.**(len-l)
+          score(i) = score(i) + real(ic) * 64.**(strlen-l)
        end do
     end do
     
-    call sorted_index_list(n,dble(score),indx)
+    call sorted_index_list(narr, dble(score), index_list)
     
   end subroutine sort_string_array
   !*********************************************************************************************************************************
