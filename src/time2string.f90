bbf00bfe8b643214bbcd9233dda02d72dd134208
MvdS <git@vandersluys.nl>
2015-01-28 12:01:08 +0100

    Updated copyright statement

--- src/time2string.f90
+++ src/time2string.f90
@@ -1,7 +1,7 @@
 !> \file time2string.f90  Procedures to convert time to formatted text strings
 
 
-!  Copyright (c) 2002-2014  Marc van der Sluys - marc.vandersluys.nl
+!  Copyright (c) 2002-2015  Marc van der Sluys - marc.vandersluys.nl
 !   
 !  This file is part of the libSUFR package, 
 !  see: http://libsufr.sourceforge.net/
