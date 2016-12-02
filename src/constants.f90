6a2db2eccc457a893606eeced2e0e0f95f84804f
MvdS <git@vandersluys.nl>
2016-12-02 12:44:43 +0100

    Constants: rename SI physical constants pc_si_* -> si_pc_*

--- src/constants.f90
+++ src/constants.f90
@@ -159,25 +159,25 @@ module SUFR_constants
   
   ! Physical constants - SI:
   !> \brief Newton's constant, m^3 kg^-1 s^-2
-  real(double), parameter, public :: pc_si_g       =  pc_g * 1.d-3  ! 6.67259d-11
+  real(double), parameter, public :: si_pc_g       =  pc_g * 1.d-3  ! 6.67259d-11
   !> \brief Speed of light in vacuo, m s^-1
-  real(double), parameter, public :: pc_si_c       =  pc_c * 1.d-2  ! 2.99792458d8
+  real(double), parameter, public :: si_pc_c       =  pc_c * 1.d-2  ! 2.99792458d8
   
   !> \brief Atomic mass unit; (mass of C12 atom)/12, kg
-  real(double), parameter, public :: pc_si_amu     =  pc_amu * 1.d-3  ! 1.6605402d-27
+  real(double), parameter, public :: si_pc_amu     =  pc_amu * 1.d-3  ! 1.6605402d-27
   !> \brief Mass of a hydrogen atom
-  real(double), parameter, public :: pc_si_mh      =  1.007825d0*pc_si_amu
+  real(double), parameter, public :: si_pc_mh      =  1.007825d0*si_pc_amu
   
   !> \brief Boltzmann constant, J/K
-  real(double), parameter, public :: pc_si_kb      =  pc_kb * 1.d-7  ! 1.380658d-23
+  real(double), parameter, public :: si_pc_kb      =  pc_kb * 1.d-7  ! 1.380658d-23
   !> \brief Planck's constant, J s
-  real(double), parameter, public :: pc_si_hp      =  pc_hp * 1.d-7  ! 6.6260755d-34
+  real(double), parameter, public :: si_pc_hp      =  pc_hp * 1.d-7  ! 6.6260755d-34
   !> \brief Reduced Planck constant, J s
-  real(double), parameter, public :: pc_si_hbar    =  pc_si_hp/pi2
+  real(double), parameter, public :: si_pc_hbar    =  si_pc_hp/pi2
   !> \brief Radiation (density) constant, 7.56591d-15 erg cm^-3 K^-4
-  real(double), parameter, public :: pc_si_arad    =  pc_si_kb**4/((pc_si_c*pc_si_hp)**3) * 8*pi**5/15.d0
+  real(double), parameter, public :: si_pc_arad    =  si_pc_kb**4/((si_pc_c*si_pc_hp)**3) * 8*pi**5/15.d0
   !> \brief Stefan-Boltzmann constant, 5.67051d-5 erg cm^-2 K^-4 s^-1
-  real(double), parameter, public :: pc_si_sigma   =  pc_si_arad*pc_si_c*0.25d0
+  real(double), parameter, public :: si_pc_sigma   =  si_pc_arad*si_pc_c*0.25d0
   
   !> \brief ElectronVolt in J
   real(double), parameter, public :: si_eV = eV * 1.d-7  ! 1.6021766d-19
