dbdf8895580f019fd81b5478b2a0f12ba786534a
MvdS <git@vandersluys.nl>
2016-12-03 09:00:54 +0100

    Optics: add function wavelength2rgb() to convert wavelength to an RGB value
    * Optics: add function wavelength2rgb() to convert wavelength to an RGB value:
      * wavelength: 390-770nm
      * RGB: (0-1, 0-1, 0-1)

--- src/optics.f90
+++ src/optics.f90
@@ -114,6 +114,74 @@ contains
   !*********************************************************************************************************************************
   
   
-end module SUFR_OPTICS
+  !*********************************************************************************************************************************
+  !> \brief  Return an RGB-value representing the colour corresponding to a light ray with a given wavelength
+  !!
+  !! \param wl  Wavelength in nm (390-770 nm)
+  !! \param df  Dimming factor 0-1 to scale RGB values with (0-1; 0: black, 1: full colour)
+  !!
+  !! \retval  wavelength2rgb  RGB values: (0-1, 0-1, 0-1)
+  !!
+  !!
+  !! \note Assumed pure colour at centre of their wavelength bands:
+  !! Colours: Violet,   blue,   green,    yellow,    orange,     red:
+  !!          (1,0,1)  (0,0,1)  (0,1,0)   (1,1,0)   (1,0.5,0)  (1,0,0)
+  !!
+  
+  function wavelength2rgb(wl, df)
+    use SUFR_kinds, only: double
+    use SUFR_system, only: warn
+    
+    implicit none
+    real(double), intent(in) :: wl,df
+    integer, parameter :: nc = 6  ! Number of colours in the spectrum (violet, blue, green, yellow, orange, red)
+    integer :: ic
+    real(double) :: wavelength2RGB(3), xIpol,dxIpol(nc),xIpol0(nc), CBbnd(nc+1),CBctr(nc),CBdst(nc+1), RGB(3)
+    
+    ! Set the boundaries of the colour bands and compute their centres and mutual distances:
+    CBbnd = dble([390,450,492,577,597,622,770])  ! Colour-band boundaries: 1:UV-V, 2:VB, 3:BG, 4:GY, 5:YO, 6:OR, 7:R-IR (nc+1)
+    do ic=1,nc
+       CBctr(ic)  = (CBbnd(ic) + CBbnd(ic+1))/2.d0      ! Centre of a colour band
+       if(ic.gt.1) CBdst(ic) = CBctr(ic) - CBctr(ic-1)  ! Distance between colour bands
+    end do
+    CBdst(1) =  CBctr(1)-CBbnd(1)
+    CBdst(nc+1) = CBbnd(nc+1)-CBctr(nc)
+    
+    ! Convert wavelength to 0 <= x <= 4.5:
+    dxIpol = dble([0.5,1.0,1.0,0.5,0.5,0.5])
+    xIpol0 = dble([0.5,1.0,2.0,3.0,3.5,4.0])
+    xIpol = (wl-390.d0)/30.d0*0.5d0                                         ! Black to violet    xIpol: 0.0 - 0.5
+    do ic=1,nc
+       if(wl.gt.CBctr(ic))  xIpol = (wl-CBctr(ic)) / CBdst(ic+1) * dxIpol(ic) + xIpol0(ic)
+    end do
+    xIpol = min(4.5d0, max(0.d0, xIpol) )  ! xIpol should be 0 <= xIpol <= 4.5
+    
+    ! Convert xIpol to RGB.  Use black as 'invisible', i.e. UV or IR:
+    if(xIpol.le.0.5d0) then
+       RGB =                         [xIpol,           0.d0,   xIpol]       ! Black to violet    xIpol: 0.0 - 0.5
+    else if(xIpol.le.1.0d0) then
+       RGB =                         [1.d0-xIpol,      0.d0,   xIpol]       ! Violet to blue     xIpol: 0.5 - 1.0
+    else if(xIpol.le.2.0d0) then
+       RGB =                         [0.d0,        xIpol-1.d0, 2.d0-xIpol]  ! Blue to green      xIpol: 1.0 - 2.0
+    else if(xIpol.le.3.0d0) then
+       RGB =                         [xIpol-2.d0,      1.d0,   0.d0]        ! Green to yellow    xIpol: 2.0 - 3.0
+    else if(xIpol.le.3.5d0) then
+       RGB =                         [1.d0,        4.d0-xIpol, 0.d0]        ! Yellow to orange   xIpol: 3.0 - 3.5
+    else if(xIpol.le.4.0d0) then
+       RGB =                         [1.d0,        4.d0-xIpol, 0.d0]        ! Orange to red      xIpol: 3.5 - 4.0
+    else if(xIpol.le.4.5d0) then
+       RGB =                         [(4.5d0-xIpol)*2, 0.d0,   0.d0]        ! Red to black       xIpol: 4.0 - 4.5
+    else
+       call warn('SUFR_optics/wavelength2RGB(): xIpol out of bounds')
+    end if
+    
+    ! Apply dimming and return:
+    wavelength2RGB = RGB*df
+    
+  end function wavelength2rgb
+  !*********************************************************************************************************************************
+  
+  
+end module SUFR_optics
 !***********************************************************************************************************************************
 
