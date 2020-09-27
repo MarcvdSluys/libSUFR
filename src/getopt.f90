b00b834bff974fe937d67f0bc554e491bf926a11
Marc van der Sluys <git@vandersluys.nl>
2020-09-27 18:23:49 +0200

    getopt / getopt_get_command_argument(): arg: retval -> param in Doxygen.

--- src/getopt.f90
+++ src/getopt.f90
@@ -350,7 +350,7 @@ contains
   !!
   !! \param argNr  Number of the desired argument
   !!
-  !! \retval arg   Content of the desired argument (a word/string)
+  !! \param arg    Content of the desired argument (a word/string)
   
   subroutine getopt_get_command_argument(argNr, arg)
     use SUFR_system, only: quit_program_error
